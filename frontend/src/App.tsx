6ad6bbd6ea96b11b632192e7d5897b9e87d136b6
wisdown <blessedinyou@gmail.com>
2025-12-02 09:05:30 -0600

    Informe: tareas realizadas: •	2. Configuración Inicial del Proyecto. Realizado Instalación de dependencias, e instalación de la aplicación •	3. Estructura del Proyecto: realizado Se estructuro el proyecto a nivel de responsabilidad. •	Primera fase concluida

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>ERP Comercializadora LomaLinda – Frontend</h1>
+      <p>Bienvenido. Aquí irá el layout principal.</p>
+    </div>
+  );
+}
+
+export default App;
